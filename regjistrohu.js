74dc1b44c458f436a078f2efdcd8e1fac66c1e7e
A&D Coding <82700860+adcodingacademy@users.noreply.github.com>
2021-06-23 17:30:02 +0200

    Update regjistrohu.js

--- regjistrohu.js
+++ regjistrohu.js
@@ -1,12 +1,9 @@
-function check(form)/*function to check userid & password*/
-{
- /*the following code checkes whether the entered userid and password are matching*/
- if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
-  {
-    window.open('target.html')/*opens the target page while Id & password matches*/
-  }
- else
- {
-   alert("Error Password or Username")/*displays error message*/
-  }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
